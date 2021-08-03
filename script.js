f70e3823dfb6879e9a371c898bf94ad9cd16c1cb
Laudemir <nlaudemir@gmail.com>
2021-08-03 18:17:32 -0300

    criando movimento das peças

--- script.js
+++ script.js
@@ -3,32 +3,43 @@ const torres = document.querySelector("#container");
 for (let i = 0; i < 3; i++) {
   const torre = document.createElement("div");
   const obj = document.createElement("div");
-  obj.classList.add("torre");
-
+  obj.classList.add(`torre${i}`);
   torre.appendChild(obj);
-
   torres.appendChild(torre);
 }
 
-const discos = document.querySelector(".torre");
+const torre = document.querySelector(".torre0");
+const torre1 = document.querySelector(".torre1");
+const torre2 = document.querySelector(".torre2");
 
-for (let i = 2; i >= 0; i--) {
+for (let i = 4; i >= 0; i--) {
   const disco = document.createElement("div");
   const obj = document.createElement("div");
-  obj.classList.add("disco");
-  obj.style.width = i * 80 + 80 + "px";
-
+  disco.classList.add("discos");
+  obj.classList.add(`disco${i}`);
+  obj.style.width = i * 40 + 100 + "px";
   disco.appendChild(obj);
-
-  discos.appendChild(disco);
+  torre.appendChild(disco);
 }
 
-const peca = document.querySelector(".torre");
+torre.addEventListener("click", mover);
+torre1.addEventListener("click", mover);
+torre2.addEventListener("click", mover);
 
-peca.addEventListener("click", mover);
+const click = () => {};
 
 function mover(evt) {
-  if (evt.target.className === "disco") {
+  if (
+    evt.target.className === "disco0" ||
+    evt.target.className === "disco1" ||
+    evt.target.className === "disco2" ||
+    evt.target.className === "disco3" ||
+    evt.target.className === "disco4" ||
+    evt.target.className === "torre0" ||
+    evt.target.className === "torre1" ||
+    evt.target.className === "torre2"
+  ) {
     console.log(evt.target);
+    torre1.appendChild("disco0");
   }
 }
