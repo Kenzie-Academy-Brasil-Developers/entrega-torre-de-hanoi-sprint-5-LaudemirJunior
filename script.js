a5f1aa2be34b5a0284945c4136f717d06025485a
Laudemir <nlaudemir@gmail.com>
2021-08-03 20:01:28 -0300

    iniciando função de verificação

--- script.js
+++ script.js
@@ -1,45 +1,38 @@
-const torres = document.querySelector("#container");
+const container = document.querySelector("#container");
 
 for (let i = 0; i < 3; i++) {
-  const torre = document.createElement("div");
   const obj = document.createElement("div");
-  obj.classList.add(`torre${i}`);
-  torre.appendChild(obj);
-  torres.appendChild(torre);
+  obj.classList.add("torre", `torre_${i}`);
+  container.appendChild(obj);
 }
 
-const torre = document.querySelector(".torre0");
-const torre1 = document.querySelector(".torre1");
-const torre2 = document.querySelector(".torre2");
+const t1 = document.querySelector(".torre_0");
 
 for (let i = 4; i >= 0; i--) {
-  const disco = document.createElement("div");
   const obj = document.createElement("div");
-  disco.classList.add("discos");
-  obj.classList.add(`disco${i}`);
+  obj.classList.add("disco", `disco_${i}`);
   obj.style.width = i * 40 + 100 + "px";
-  disco.appendChild(obj);
-  torre.appendChild(disco);
+  t1.appendChild(obj);
 }
 
-torre.addEventListener("click", mover);
-torre1.addEventListener("click", mover);
-torre2.addEventListener("click", mover);
+const torre = document.querySelectorAll(".torre");
 
-const click = () => {};
+let selecionado = false;
+let discoSelecionado;
 
-function mover(evt) {
-  if (
-    evt.target.className === "disco0" ||
-    evt.target.className === "disco1" ||
-    evt.target.className === "disco2" ||
-    evt.target.className === "disco3" ||
-    evt.target.className === "disco4" ||
-    evt.target.className === "torre0" ||
-    evt.target.className === "torre1" ||
-    evt.target.className === "torre2"
-  ) {
-    console.log(evt.target);
-    torre1.appendChild("disco0");
+torre.forEach((elemento) => {
+  elemento.addEventListener("click", moverDiscos);
+});
+
+function moverDiscos(event) {
+  let torreSelecionada = event.currentTarget;
+  let total = torreSelecionada.childElementCount;
+
+  if (selecionado === false && total > 0) {
+    selecionado = true;
+    discoSelecionado = torreSelecionada.children[total - 1];
+  } else if (selecionado === true) {
+    torreSelecionada.appendChild(discoSelecionado);
+    selecionado = false;
   }
 }
